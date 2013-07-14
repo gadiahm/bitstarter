b0dbbcc02b54a725fbce6aa9e1584cebd8a48773
Ahmad Gadi <gadi.ahm@gmail.com>
2013-07-14 00:54:05 +0000

    test

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 var content = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(content);
+  response.send("hello");
 });
 
 var port = process.env.PORT || 5000;
